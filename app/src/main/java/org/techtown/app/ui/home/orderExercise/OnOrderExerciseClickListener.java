f72b4e1c9728b5052d67bba7343d3583df0dbaba
soy <soysauce1223@naver.com>
2022-11-20 22:44:36 +0900

    HealthCure

---
+++ app/src/main/java/org/techtown/app/ui/home/orderExercise/OnOrderExerciseClickListener.java
@@ -0,0 +1,7 @@
+package org.techtown.app.ui.home.orderExercise;
+
+import android.view.View;
+
+public interface OnOrderExerciseClickListener {
+    public void onItemClick(OrderExerciseAdapter.ViewHolder holder, View view, int position);
+}
